1d27bfa1118a4a91cecdb2329ca4ea0da328bdf8
llerey <llerey.yt@gmail.com>
2022-01-23 15:25:00 +0300

    Create termux-one-tools.sh

---
+++ termux-one-tools.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+apt update
+
+apt upgrade
+
+apt install python3
+
+apt install kotlin
+
+apt install neovim
+
+apt install vim
+
+apt install lsd
+
+apt install nodejs
+
+apt install ffmpeg
+
+apt install proot
+
+apt install openssh
+
+apt install git
+
+apt install tsu
+
+termux-setup-storage
+
+apt install bash-completion
+
+apt install man
+
+apt install imagemagick
+
+apt install mc
+
